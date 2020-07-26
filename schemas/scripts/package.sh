bed98dbf434fb30519b9afe91d7a6a2357a875e4
bipinupd <bipinupd@gmail.com>
2020-07-26 18:32:19 -0400

    New table

--- schemas/scripts/package.sh
+++ schemas/scripts/package.sh
@@ -2,15 +2,11 @@
 mkdir -p /workspace/schemas/
 cd /workspace/schemas/
 xargs mkdir -p < /workspace/dir-structure.txt
-echo "&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&"
-cat /workspace/schema-diff.txt
-echo "&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&"
 while read line;
 do
-    echo "/workspace/datalake-repo/$line ------------/workspace/$line"
     cp "/workspace/datalake-repo/$line" "/workspace/$line"
 done < /workspace/schema-diff.txt
-cp /workspace/datalake-repo/schemas/scripts/* /workspace/schemas/scripts/
+cp -R /workspace/datalake-repo/schemas/scripts/ /workspace/schemas/scripts/
 cp /workspace/datalake-repo/schemas/cloudbuild.yaml /workspace/schemas/cloudbuild.yaml
 cp /workspace/datalake-repo/schemas/cloudbuild-deploy.yaml /workspace/schemas/cloudbuild-deploy.yaml
 ls -la /workspace/schemas/*
