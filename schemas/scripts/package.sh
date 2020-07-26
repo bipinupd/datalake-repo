cfcf04c09a48e812e7b9d5e16c0f8f1b24ed31a1
bipinupd <bipinupd@gmail.com>
2020-07-26 17:42:26 -0400

    New table

--- schemas/scripts/package.sh
+++ schemas/scripts/package.sh
@@ -3,15 +3,10 @@ mkdir -p /workspace/schemas/
 cd /workspace/schemas/
 xargs mkdir -p < /workspace/dir-structure.txt
 cd /workspace/datalake-repo/
-input="/workspace/schema-diff.txt"
-cat "$linput"
 while read line;
-echo "################"
 do
-    pwd
-    echo "$line"
     cp "$line" "/workspace/$line"
-done < "$input"
+done < /workspace/schema-diff.txt
 cp -R schemas/scripts /workspace/schemas
 cp schemas/cloudbuild.yaml /workspace/schemas/cloudbuild.yaml
 cp schemas/cloudbuild-deploy.yaml /workspace/schemas/cloudbuild-deploy.yaml
