ebf22be473b73672b2739c9fef985251b05ca86c
bipinupd <bipinupd@gmail.com>
2020-07-26 22:05:58 -0400

    New table

--- dlp/tests/it-test/inspect_template_example2_test.py
+++ dlp/tests/it-test/inspect_template_example2_test.py
@@ -8,7 +8,7 @@ class GenericInspectForExample2Test:
     def setup_class(self):
         self.template_id = "generic_inspect_template_example2"
 
-    def test_inspect_email(self, project):
-        item = {'value': 'My email is test@example.com'}
+    def test_inspect_gender(self, project):
+        item = {'value': 'The victim was male'}
         response = common.inspectdata(item, project,self.template_id)
-        assert response.result.findings[0].info_type.name == "EMAIL_ADDRESS"
+        assert response.result.findings[0].info_type.name == "GENDER"
